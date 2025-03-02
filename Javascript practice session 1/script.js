e41dfbc88fa57bd0ca143129a9c6ed6dc1ccb425
Manas Singh <manasinghofficial@gmail.com>
2025-03-02 13:22:04 +0530

    commit

---
+++ Javascript practice session 1/script.js
@@ -0,0 +1,69 @@
+//? Log "Hello, JavaScript!" to the console in 3 different ways.
+
+// console.log('Hello World');
+// console.warn('Hello World');
+// console.error('Hello World');
+// console.info('Hello World');
+// console.table({name:'harsh'});
+
+//? 2. Perform 35 * 2 - (10 / 2) + 7 and log the result.
+
+// let calc = 35 * 2 - (10 / 2) + 7
+// console.log(calc)
+
+//? 3. Log the data type of "123", 123, true, and null using typeof.
+
+// console.log(typeof '123');
+// console.log(typeof 123);
+// console.log(typeof true);
+// console.log(typeof null);
+ 
+//? 4. Write a program that swaps the values of two variables.
+
+// let a = 12;
+// let b = 13;
+// let c 
+// console.log(a,b);
+// c = a;
+// a = b;
+// b = c;
+// console.log(a,b);
+
+//? 5. Use console.group() to organize logs into a group.
+
+// console.group("User Info");
+// console.log("Name: John Doe");
+// console.log("Age: 25");
+// console.log("City: New York");
+// console.groupEnd();
+
+// console.groupCollapsed("Collapsed Group Example");
+// console.log("This content is collapsed by default.");
+// console.groupEnd();
+
+//? 6. Declare a const object, modify its properties, and log the updated object.
+
+// const obj = {name:'manas', age:21, male:true}
+// obj.age = 20
+// console.log(obj.age);
+
+//? 7. Convert "50" (string) into a number using 3 different methods.
+
+// let num = '50'
+// Number('50')
+// parseInt('50')
+// +'50'
+
+//? 8. Check if "JavaScript" contains "Script" without using .includes().
+
+// let str = "JavaScript";
+// console.log(str.indexOf('Script') !== -1);
+
+//? 9. Create an array of 5 numbers and log the sum using .reduce().
+
+// let arr = [1,2,3,4,5]
+// let sum = arr.reduce(add)
+// console.log(sum);
+
+
+//? 10. Explain the difference between undefined, null, and NaN with examples.
