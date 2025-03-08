35ca3736d7b8b593201269ddffca36bd4d514aad
Manas Singh <manasinghofficial@gmail.com>
2025-03-08 10:33:17 +0530

    day 27

---
+++ DSA/day27.js
@@ -0,0 +1,46 @@
+//! 1. Increment and Decrement Operators
+
+//? Q1
+// let i = 11;
+// i = i++ + ++i;
+// console.log(i); //24
+
+//? Q2
+// let a =11,b=22,c;
+// c= a + b + a++ + b++ + ++a + ++b;
+// console.log('a='+a); //13
+// console.log('b='+b); //24
+// console.log('c='+c); //103
+
+//? Q3
+// let i =0;
+// i = i++ - --i + ++i - i--;
+// console.log(i); //0
+
+//? Q4
+// let b =true;
+// b++;
+// console.log(b); //2
+
+//! 2 Complex Increment and Decrement Qperations
+
+//? Q5
+// let i = 1 , j= 2, k=3;
+// let m=i-- - j-- - k--;
+// console.log('i='+i); //0
+// console.log('j='+j); //1
+// console.log('k='+k); //2
+// console.log('m='+m); //-4
+
+//? Q6
+let a =1; b=2
+console.log(--b - ++a + ++b - --a); //0
+
+//? Q7
+let i = 19, j=29, k;
+
+//? Q8
+//? Q9
+//? Q10
+//? Q11
+//? Q12
---
+++ DSA/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let a =11,b=22,c;
+// c= a + b + a++ + b++ + ++a + ++b;
+// console.log('a='+a); //13
+// console.log('b='+b); //24
+// console.log('c='+c); //103
