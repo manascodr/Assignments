dade6c3815072cf827b72fd7b5ca8831cb1aa36d
Manas Singh <manasinghofficial@gmail.com>
2025-06-20 08:41:35 +0530

    c

--- Day-51 strings/script.js
+++ Day-51 strings/script.js
@@ -1,6 +1,6 @@
-   // Check weather the input string is a palindrome  
-   
-   //first approach
+//? Check weather the input string is a palindrome
+
+//first approach
 //    function palindrome(str){
 //         let strLow = str.toLowerCase()
 //         let rev = ''
@@ -16,6 +16,15 @@
 
 //    console.log(palindrome('madam'));
 
+//? Count strings with given prefix
 
-// 2 pointer approach
+// let words = ["pay", "attention", "practice", "attend"];
+// let pref = "at";
+// let count = 0;
 
+// for (let i = 0; i < words.length; i++) {
+//   if (words[i].startsWith(pref)) {
+//     count++;
+//   }
+// }
+// console.log(count);
---
+++ Strings questions/app.js
@@ -0,0 +1,41 @@
+//? Program for Sorting first half in Ascending order and second half in Descending order
+// let arr = [3, 2, 4, 1, 10, 30, 40, 20]
+// let n = arr.length
+
+// for (let i = 0; i < n-1; i++) {
+//     for (let j = 0; j < n/2; j++) {
+
+//         if (arr[j]> arr[j+1]) {
+//             let temp = arr[j]
+//             arr[j] = arr[j+1]
+//             arr[j+1] = temp
+//         }
+        
+//     }
+         
+//     for (let j = n/2; j < n-1; j++) {
+//             if (arr[j]< arr[j+1]) {
+//             let temp = arr[j]
+//             arr[j] = arr[j+1]
+//             arr[j+1] = temp
+//         }
+        
+//     }
+// }
+// console.log(arr);
+
+
+//? Toggle Alphabet Case
+// let str = 'hElLo'
+// let ans = ''
+// for (let i = 0; i < str.length; i++) {
+//     let asci = str.charCodeAt(i);
+//     if (asci>=65 && asci<=90) {
+//         ans += str.charAt(i).toLowerCase()
+//     }
+//     else if (asci>97 && asci<=122) {
+//         ans += str.charAt(i).toUpperCase()
+//     }
+    
+// }
+// console.log(ans);
